93ccddf586b2b96d4f01347642a1d49f8d4dea50
Joshua Ho <ho.joshua4@gmail.com>
2019-09-05 22:54:50 -0700

    Added comments to code for conceptual understanding

--- challenges/longestUniqueString/challenge.py
+++ challenges/longestUniqueString/challenge.py
@@ -5,14 +5,20 @@ class Solution:
         for char in s:
             if char in track:
                 while True:
+                    #
                     if track[0] == char:
                         track = track[1:]
                         track += char
                         break
+                    #If the character is found in 'track', remove the first character from track until
+                    #char == track[0].  Once the character is removed, you are at the beginning of the
+                    #next longest string
                     else:
                         track = track[1:]
             else:
                 track += char
+            #Regardless of the current length of track, the longest length of track will be stored here
+            #and returned as your answer.
             if len(track) > output:
                 output = len(track)
         print('track:', track)
