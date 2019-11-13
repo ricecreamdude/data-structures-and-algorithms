9ef9a949daeab625d6bf9af44f14e86c03a9bde9
Joshua Ho <ho.joshua4@gmail.com>
2019-11-12 16:31:56 -0800

    Working custom paths algorithm

--- data-structures/pathfinding/findAllUniquePaths.js
+++ data-structures/pathfinding/findAllUniquePaths.js
@@ -1,1 +1,36 @@
 // https://www.geeksforgeeks.org/unique-paths-in-a-grid-with-obstacles/
+
+
+let uniquePaths = (m,n) => {
+
+  let aux = createGrid(m,n);
+
+  for(let i = 1; i < m; i++){
+    for (let j = 1; j < n; j++){
+      aux[i][j] = aux[i-1][j] + aux[i][j-1];
+    }
+  }
+
+
+
+  return aux[m-1][n-1];
+
+}
+
+let createGrid = (x,y) => {
+
+  let grid = [];
+
+  for (let i = 0; i < x; i++){
+    grid.push([]);
+    for (let j = 0; j < y; j++){
+      grid[i].push(1);
+    }
+  }
+
+
+  return grid;
+}
+
+
+console.log('SHOULD EQUAL 20:' ,uniquePaths(4,4)); 
